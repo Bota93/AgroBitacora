9efa2e4a5cc5a2f909fd70f135dddcba41d31860
Bota93 <a.alcaraz.rodri@gmail.com>
2025-06-12 13:37:26 +0200

    Primer commit: interfaz inicial Tailwind y tareas básicas

---
+++ src/data/tareas.js
@@ -0,0 +1,5 @@
+export const tareas = [
+    {id: 1, texto: "Revisar riego de limoneros", completada: false},
+    {id: 2, texto: "Aplicar fertilizante en zona 2", completada: true},
+    {id: 3, texto: "Preparar herramientas para la poda", completada: false}
+]
